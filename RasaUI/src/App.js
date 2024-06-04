11b72ba40be367fb68f95bc534f8b23fc8119ede
Shreyeska <silwalshreyeska@gmail.com>
2024-06-04 23:38:41 +0545

    UI frontend added

---
+++ RasaUI/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Basic from "./components/Basic"
+
+function App() {
+  return (
+    <div className="App">
+ 
+      <h1> ImePay</h1>
+
+      <Basic />
+    </div>
+  );
+}
+
+export default App;
